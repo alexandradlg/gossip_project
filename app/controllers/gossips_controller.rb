9b1c48e1498aeb2ac2cee3409ef1b501a88abda7
Alexandra DLG <adelagueronniere@gmail.com>
2018-04-30 20:23:04 +0200

    gossip ok ext step comment

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,11 +1,12 @@
 class GossipsController < ApplicationController
 
   def new
+    @gossip = Gossip.new
     
   end
 
   def create
-    @gossip = Gossip.new(anonymous_author: params[:anonymous_author], content: params[:content])
+    @gossip = Gossip.new(gossip_params)
     if @gossip.save
         redirect_to gossip_path(@gossip.id)
     else
@@ -14,8 +15,33 @@ class GossipsController < ApplicationController
   end
 
   def show
-    
+    @gossip = Gossip.find(params[:id])
   end
   
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+  
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(gossip_params)
+    redirect_to gossip_path
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
+  end
+
+  def index
+    @gossips = Gossip.all
+  end
+  
+
+  private
+  def gossip_params
+    params.permit(:anonymous_author, :content)
+  end
   
 end
